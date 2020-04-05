9c35dcae2450c1cdc2fab59bac894e541a36261f
Vladimir Taranovskiy <8756136@gmail.com>
2020-04-06 00:12:14 +0300

    MiniCssExtractPlugin added. Cross-env added.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
 module.exports = {
   mode: 'development',
@@ -27,11 +31,15 @@ module.exports = {
     }
   },
   devServer: {
-    port: 4200
+    port: 4200,
+    hot: isDev,
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      }
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
@@ -40,12 +48,24 @@ module.exports = {
         to: path.resolve(__dirname, 'dist'),
       }
     ]),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
